3d5a12cc3eeefba27799dcfc9437562d08fe80dd
grace <massive2bdamnedfan@gmail.com>
2026-03-20 17:59:30 +0000

    move roblosec to settings.env (webhook due)

--- core/utilities/assetdeliverer.php
+++ core/utilities/assetdeliverer.php
@@ -41,6 +41,7 @@
 	$settings = parse_ini_file($_SERVER['DOCUMENT_ROOT']."/../settings.env", true);
 
 	$access = $settings['asset']['ACCESSKEY'];
+	$roblosec = $settings['asset']['ROBLOSEC'];
 
 	require_once $_SERVER['DOCUMENT_ROOT']."/core/utilities/clientdetect.php";
 	require_once $_SERVER['DOCUMENT_ROOT']."/core/utilities/userutils.php";
@@ -159,7 +160,7 @@
 		if(!file_exists($_SERVER['DOCUMENT_ROOT']."/../assets/rbx_".$id.(isset($_GET['version']) ?  "_".$version : ""))) {
 			$url = "https://assetdelivery.roblox.com/v1/asset/?id=".$id.(isset($_GET['version']) ? '&version='.$version : "");
 			$ch = curl_init ($url);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, array("Cookie: .ROBLOSECURITY=_|WARNING:-DO-NOT-SHARE-THIS.--Sharing-this-will-allow-someone-to-log-in-as-you-and-to-steal-your-ROBUX-and-items.|_CAEaAhADIhwKBGR1aWQSFDEzMDE4NzM0Nzk4MTY5MzI2NzM3KAM.96IxbhII0plfO0-sutwtWcZ6VGtGpUkrb_qA4-7erYDzkobQOxhyPmagepO2qf-k7Cg4JcYtwS2md-cb-5578IPsFclIzQNPNaGMxMKrXokv5IB-bI_3RWKg-sgMw69GWpr51wdf5K9Kya07CbiXAMome47HcAoUUvfdTzvobPLluzxstvCQ2JLryJH3diLQBQMpn8aslB4z4jHcOfHCBReitSz2ognLYJ2ytVFwVKAY-BALtRnbblwSAIX2b6UDVtizP3HRSh0vLhpIYuY3X4RoR0wtI8bdXpFkrj3oRZlSMLfuBobC3fB4Ou8Y92uroPguQSAVxx3SMDFSYzhcokztPirv_vL7ToiLsxYb_5M5InFA1NpM65_PxowFBbAFWWhsiMz2t62t0_TcUqD6lPyypxEN9auJgVdARFv0wtof_9KbuOjhK23pMPuwKuDTMSpDo0jgdhe10b2yN3TYVcnaq2itXzikJh04kjKAl6cJ7oWWh90NaEBUtEdUnk-zpA4T7hM3e8Qg1XR-mMccz63kN509NO2yb2-8zRZ1WzDuWDrYZPrhQKcZ1qQtHs9Jy3S2fa-hsvvo-aw2n034UPchOc6VlroAVU-2e8C2Wgu1eLE_dRgnKr4v0MmjSSvaC5NK-DMKHrlSFaD5deWhLhI7bU0YdwH2DG-J4L-g2QSH9rrh24WBWPyAZgsk0ei0b8VJ5vCUU0OcPlrggx9KMKqTkjjHzCXYftp3wwwgKLsIPbxW07dz2NUqsH59gyDRiUpOwITvm3u6AvGVSG4R2VmIlkyLnmH4h38NZ2QAagWyz0EY"));
+			curl_setopt($ch, CURLOPT_HTTPHEADER, array("Cookie: .ROBLOSECURITY=$roblosec"));
 			curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
