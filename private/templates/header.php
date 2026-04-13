86343cddb492989eb036b4a595df16d7de74ff23
2BEHDAMNED <massive2bdamnedfan@gmail.com>
2026-04-13 10:44:44 +0100

    test

--- private/templates/header.php
+++ private/templates/header.php
@@ -86,11 +86,37 @@
 		<script>
 			const wait = (delay = 0) =>	new Promise(resolve => setTimeout(resolve, delay));
 
+			function waitForElm(selector) {
+				return new Promise(resolve => {
+					if (document.querySelector(selector)) {
+						return resolve(document.querySelector(selector));
+					}
+
+					const observer = new MutationObserver(mutations => {
+						if (document.querySelector(selector)) {
+							observer.disconnect();
+							resolve(document.querySelector(selector));
+						}
+					});
+
+					// If you get "parameter 1 is not of type 'Node'" error, see https://stackoverflow.com/a/77855838/492336
+					observer.observe(document.body, {
+						childList: true,
+						subtree: true
+					});
+				});
+			}
+
 			function setVisible(element, visible) {
 				if(element == "#LoadingScreen")
-					document.querySelector(element).style.display = visible ? "flex" : "none";
+					waitForElm(element).then((elm) => {
+						elm.style.display = visible ? "flex" : "none";
+					});
+					
 				else
-					document.querySelector(element).style.display = visible ? "block" : "none";
+					waitForElm(element).then((elm) => {
+						elm.style.display = visible ? "flex" : "none";
+					});
 			}
 
 			// do loading screen if the page hasn't loaded in a second.
