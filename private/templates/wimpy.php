047b3efee947d0a168d16ebb6cdbf91a222a12e3
grace <massive2bdamnedfan@gmail.com>
2026-05-17 11:24:42 +0100

    STUPID FUCKING HACK BECAUSE WIMPY IS A FAT FUCKING BITCH FUCK YOU

--- private/templates/wimpy.php
+++ private/templates/wimpy.php
@@ -5,7 +5,7 @@
 		data-loop="2"
 		data-disablecontrols="next,playlist,rewind,getid3"
 		style="text-align: center;"
-		data-media="<?= $this->url ?>"
+		data-media="<?= $this->url ?>.mp3"
 		data-volume="0.4"
 	></div>
 	<div style="border: 2px solid black; background: #222; color: white; padding: 5px; text-align: center; margin-left:-2px">
